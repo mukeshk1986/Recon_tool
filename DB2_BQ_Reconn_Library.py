8163f1d8b7073f77225336ff027b4c378d145a61
Mukesh Kumar <42538599+mukeshk1986@users.noreply.github.com>
2023-10-20 13:11:49 -0700

    Update DB2_BQ_Reconn_Library.py
    First Commit

--- DB2_BQ_Reconn_Library.py
+++ DB2_BQ_Reconn_Library.py
@@ -1,1 +1,7 @@
-
+class CDM_Compare:
+  def __init__(self, V_Conf_Project, V_Conf_Dataset, V_Conf_Table, V_Source_Count, V_Conf_New_Table):
+    self.V_Conf_Project=
+    self.V_Conf_Dataset, 
+    self.V_Conf_Table, 
+    self.V_Source_Count, 
+    self.V_Conf_New_Table
